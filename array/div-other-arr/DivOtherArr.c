2dc42f6a8aa8c091a0ffa8bea3a6a7c1b209884b
jidazhou <1443954561@qq.com>
2024-09-25 23:55:26 +0800

    add DivOtherArr

--- array/div-other-arr/DivOtherArr.c
+++ array/div-other-arr/DivOtherArr.c
@@ -5,18 +5,23 @@
 #include <malloc.h>
 
 int main() {
-    int arr[] = {5,4,-1,7,8};
+    int arr[] = {2,4,1,0,3};
     int length = sizeof(arr) / sizeof(typeof(*arr));
 
     int *pre = malloc(sizeof(arr));
     pre[0] = 1;
-    for (int i = 1; i < sizeof(arr) / sizeof(typeof(*arr)); i++) {
+    for (int i = 1; i < sizeof(arr) / sizeof(typeof(*arr)); ++i) {
         pre[i] = pre[i - 1] * arr[i - 1];
     }
 
     int *suf = malloc(sizeof(arr));
     suf[length-1] = 1;
-    for (int i = length - 2; i > 0; i++) {
+    for (int i = length - 2; i >= 0; --i) {
         suf[i] = suf[i + 1] * arr[i + 1];
     }
+
+    for (int i = 0; i < length; i++) {
+        printf("%d ", pre[i] * suf[i]);
+    }
+
 };

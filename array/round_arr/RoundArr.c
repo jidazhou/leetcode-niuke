82bc35ade2faefc393aed50af4ae1b7902d2dede
jidazhou <102165712+jidazhou@users.noreply.github.com>
2024-09-30 18:04:44 +0800

    create RoundArr

--- array/round_arr/RoundArr.c
+++ array/round_arr/RoundArr.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
+#include <malloc.h>
 
 int main(){
 	int arr[] = {1, 2, 3, 4, 5, 6};
 	int length = sizeof(arr)/sizeof(int);
-	
-	int arrOut[length] = {0};
+	int *arrOut = malloc(sizeof(arr));
 	
 	int t = 0;
-	scantf("%d", &t);
+	scanf("%d", &t);
 	printf("times of rounds：%d", t);
 	
 	for (int i = 0; i < length; i++){
@@ -15,7 +15,7 @@ int main(){
 	}
 	
 	for (int i = 0; i < length; i++){
-		printf("%d ", arrOut(i));
+		printf("%d ", arrOut[i]);
 	}
 
 	return 0;
